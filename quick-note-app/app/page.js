64d74f12172fdce1e1b6a9955bc4033d4935be3e
Emrah SÖZLÜ <emrahsozlu@aof.anadolu.edu.tr>
2023-09-29 12:44:49 +0300

    edit done

--- quick-note-app/app/page.js
+++ quick-note-app/app/page.js
@@ -3,15 +3,18 @@ import styles from './page.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { useEffect, useState } from 'react';
 import NavigationBar from '@/components/NavigationBar';
-import { Button, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, ListGroup, Row, Offcanvas } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faSave, faTrash } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 export default function Home() {
-  const apiUrl = "https://localhost:7061/api/Notes";
+  const apiUrl = "https://quicknoteapi.emrahsozlu.com/api/Notes";
   const [notes, setNotes] = useState([]);
   const [selectedNote, setSelectedNote] = useState(null);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     fetch(apiUrl)
@@ -21,6 +24,7 @@ export default function Home() {
 
   const itemClicked = (e, note) => {
     setSelectedNote(note);
+    handleClose();
   };
 
   const addNewNote = (e) => {
@@ -33,6 +37,7 @@ export default function Home() {
       setNotes([...notes, response.data]);
       setSelectedNote(response.data);
       toast("New note created.");
+      handleClose();
     });
   };
 
@@ -45,7 +50,7 @@ export default function Home() {
   };
 
   const saveNote = (e) => {
-    axios.put(apiUrl + "/" + selectedNote.id, selectedNote).then(function(response) {
+    axios.put(apiUrl + "/" + selectedNote.id, selectedNote).then(function (response) {
       const newNotes = [...notes];
       let i = notes.findIndex(n => n.id == selectedNote.id);
       newNotes[i] = selectedNote;
@@ -54,38 +59,44 @@ export default function Home() {
     });
   };
 
+  const leftColumn = (
+    <>
+      <div className='d-flex justify-content-end'>
+        <Button className='mt-3' onClick={addNewNote}>
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+      </div>
+      <ListGroup className='mt-2'>
+        {
+          notes.map(note => (
+            <ListGroup.Item key={note.id} action active={note.id == selectedNote?.id} onClick={(e) => itemClicked(e, note)}>
+              {note.title}
+            </ListGroup.Item>
+          ))
+        }
+      </ListGroup>
+    </>
+  );
+
   return (
     <main className={styles.main}>
-      <NavigationBar />
-      <Container>
-        <Row>
-          <Col sm={5} md={4} lg={3}>
-            <div className='d-flex justify-content-end'>
-              <Button className='mt-3' onClick={addNewNote}>
-                <FontAwesomeIcon icon={faPlus} />
-              </Button>
-            </div>
-            <ListGroup className='mt-2'>
-              {
-                notes.map(note => (
-                  <ListGroup.Item key={note.id} action active={note.id == selectedNote?.id} onClick={(e) => itemClicked(e, note)}>
-                    {note.title}
-                  </ListGroup.Item>
-                ))
-              }
-            </ListGroup>
+      <NavigationBar onBarsClick={handleShow} />
+      <Container className='flex-fill'>
+        <Row className='h-100'>
+          <Col className='d-none d-sm-block' sm={5} md={4} lg={3}>
+            {leftColumn}
           </Col>
           {selectedNote &&
-            <Col sm={7} md={8} lg={9}>
+            <Col sm={7} md={8} lg={9} className='d-flex flex-column'>
               <div className='mt-3'>
                 <Form.Control placeholder='Title' value={selectedNote?.title}
                   onChange={(e) => setSelectedNote({ ...selectedNote, title: e.target.value })} />
               </div>
-              <div className='mt-2'>
+              <div className='mt-2 flex-fill'>
                 <Form.Control as="textarea" placeholder='Your note...' rows={10} value={selectedNote?.content}
-                 onChange={(e) => setSelectedNote({ ...selectedNote, content: e.target.value })} />
+                  onChange={(e) => setSelectedNote({ ...selectedNote, content: e.target.value })} className='h-100' />
               </div>
-              <div className='mt-2'>
+              <div className='mt-2 mb-3'>
                 <Button variant='success' onClick={saveNote}>
                   <FontAwesomeIcon icon={faSave} /> Save
                 </Button>
@@ -98,6 +109,14 @@ export default function Home() {
         </Row>
       </Container>
       <ToastContainer autoClose={1000} position='bottom-right' />
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>QuickNote</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          {leftColumn}
+        </Offcanvas.Body>
+      </Offcanvas>
     </main>
   )
 }
--- quick-note-app/components/NavigationBar.js
+++ quick-note-app/components/NavigationBar.js
@@ -1,19 +1,24 @@
-import { Container, Navbar } from "react-bootstrap";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Container, Navbar } from "react-bootstrap";
 
-export default function NavigationBar() {
-    return(
-        <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              alt=""
-              src="/logo.png"
-              height="24"
-              className="d-inline-block align-top"
-            />{' '}
-            Quick Note
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    );
+export default function NavigationBar({ onBarsClick }) {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container className="justify-content-start">
+        <Button className="me-2 d-sm-none" variant="dark" onClick={onBarsClick} >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src="/logo.png"
+            height="24"
+            className="d-inline-block align-top"
+          />{' '}
+          Quick Note
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 }
