aa27dbffdb2b7511e41cd093ded3254b21739e87
Emrah SÖZLÜ <emrahsozlu@aof.anadolu.edu.tr>
2023-09-29 10:58:17 +0300

    crud done

--- quick-note-app/app/page.js
+++ quick-note-app/app/page.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import NavigationBar from '@/components/NavigationBar';
 import { Button, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faSave, faTrash } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 export default function Home() {
@@ -29,13 +29,30 @@ export default function Home() {
       content: ""
     };
 
-    axios.post(apiUrl, newNote).then(function(response) {
+    axios.post(apiUrl, newNote).then(function (response) {
       setNotes([...notes, response.data]);
       setSelectedNote(response.data);
+      toast("New note created.")
     });
   };
 
+  const deleteNote = (e) => {
+    axios.delete(apiUrl + "/" + selectedNote.id).then(function (response) {
+      setNotes(notes.filter(n => n.id != selectedNote.id));
+      setSelectedNote(null);
+      toast("Note deleted.")
+    });
+  };
 
+  const saveNote = (e) => {
+    axios.put(apiUrl + "/" + selectedNote.id, selectedNote).then(function(response) {
+      const newNotes = [...notes];
+      let i = notes.findIndex(n => n.id == selectedNote.id);
+      newNotes[i] = selectedNote;
+      setNotes(newNotes);
+      toast("Note saved.")
+    });
+  };
 
   return (
     <main className={styles.main}>
@@ -44,31 +61,43 @@ export default function Home() {
         <Row>
           <Col sm={5} md={4} lg={3}>
             <div className='d-flex justify-content-end'>
-            <Button className='mt-3' onClick={addNewNote}>
-              <FontAwesomeIcon icon={faPlus} />
-            </Button>
+              <Button className='mt-3' onClick={addNewNote}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
             </div>
             <ListGroup className='mt-2'>
               {
                 notes.map(note => (
-                  <ListGroup.Item key={note.id} action active={note == selectedNote} onClick={(e) => itemClicked(e,note)}>
+                  <ListGroup.Item key={note.id} action active={note.id == selectedNote?.id} onClick={(e) => itemClicked(e, note)}>
                     {note.title}
                   </ListGroup.Item>
                 ))
               }
             </ListGroup>
           </Col>
-          <Col sm={7} md={8} lg={9}>
-            <div className='mt-3'>
-              <Form.Control placeholder='Title' value={selectedNote?.title} />
-            </div>
-            <div className='mt-2'>
-              <Form.Control as="textarea" placeholder='Your note...' rows={10} value={selectedNote?.content} />
-            </div>
-          </Col>
+          {selectedNote &&
+            <Col sm={7} md={8} lg={9}>
+              <div className='mt-3'>
+                <Form.Control placeholder='Title' value={selectedNote?.title}
+                  onChange={(e) => setSelectedNote({ ...selectedNote, title: e.target.value })} />
+              </div>
+              <div className='mt-2'>
+                <Form.Control as="textarea" placeholder='Your note...' rows={10} value={selectedNote?.content}
+                 onChange={(e) => setSelectedNote({ ...selectedNote, content: e.target.value })} />
+              </div>
+              <div className='mt-2'>
+                <Button variant='success' onClick={saveNote}>
+                  <FontAwesomeIcon icon={faSave} /> Save
+                </Button>
+                <Button variant='danger' onClick={deleteNote} className='ms-2'>
+                  <FontAwesomeIcon icon={faTrash} /> Delete
+                </Button>
+              </div>
+            </Col>
+          }
         </Row>
       </Container>
-      <ToastContainer />
+      <ToastContainer autoClose={1000} position='bottom-right' />
     </main>
   )
 }
